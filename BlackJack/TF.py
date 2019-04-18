e7f4fb61e92c131e62ba8333bc0d527a60cf908e
Kevin Hue <khue@fragomen.com>
2019-04-18 14:11:34 -0700

    Various updates

---
+++ BlackJack/TF.py
@@ -0,0 +1,7 @@
+'''
+The game needs to have one player versus an automated dealer.
+The player can stand or hit.
+The player must be able to pick their betting amount.
+You need to keep track of the player's total money.
+You need to alert the player of wins, losses, or busts, etc...
+'''
